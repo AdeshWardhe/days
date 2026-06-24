f1440f53c79cb3e47f5a8603c17b1677bf694e25
Adesh Wardhe <wardheadesh92@gmail.com>
2026-06-24 16:44:15 +0530

    Improve portal alignment and hero animations

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// const door = document.querySelector(".door");
+
+// let direction = 1;
+
+// setInterval(() => {
+
+//     const current =
+//         parseFloat(
+//             getComputedStyle(door)
+//             .transform.split(",")[5]
+//         ) || 0;
+
+//     door.style.transform =
+//         `translateY(${direction * 8}px)`;
+
+//     direction *= -1;
+
+// }, 2000);
